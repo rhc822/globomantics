6ae548a33283f39b82620b35b4876096637cbfb0
Ryan Cunningham <rhc822@gmail.com>
2021-11-22 08:56:55 -0600

    Working on module 2: Exploring the Project Structure of ASP.NET Core 3

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using Globomantics.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,9 @@ namespace Globomantics
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
+            services.AddSingleton<IConferenceService, ConferenceApiService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
